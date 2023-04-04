b5640c4545eaa59e2c4a86efede4940e51b95420
Suriya R <suriyasparks007@gmail.com>
2023-04-04 23:23:49 +0530

    Converter App

---
+++ CounterApp/app/src/main/java/com/example/counterapp/MainActivity.java
@@ -0,0 +1,25 @@
+package com.example.counterapp;
+
+import android.os.Bundle;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
+
+import com.example.counterapp.databinding.ActivityMainBinding;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(savedInstanceState);
+
+    }
+
+}
